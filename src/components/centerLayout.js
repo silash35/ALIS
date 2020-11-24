29afc07061a1159f6f8fbe2c747a4bbf33f72f7e
Silas Henrique <silash35@gmail.com>
2020-11-24 20:34:42 -0300

    start to develop ALIS

--- src/components/centerLayout.js
+++
@@ -1,9 +0,0 @@
-import style from './centerLayout.module.scss';
-
-export default function CenterLayout(props) {
-  return (
-    <div className={style.CenterLayout}>
-      {props.children}
-    </div>
-  )
-}
