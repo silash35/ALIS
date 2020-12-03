30b6b83e7b7a1d7da4f74e2de005adc01009c14c
Silas Henrique <silash35@gmail.com>
2020-12-03 11:14:40 -0300

    add twitter card

--- src/components/defaultHead.js
+++ src/components/defaultHead.js
@@ -3,7 +3,23 @@ import Head from "next/head";
 export default function DefaultHead(props) {
   return (
     <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="content-type" content="text/html" charset="UTF-8" />
+      <meta
+        name="description"
+        content="O Agregador de Locais Inclusivos para Surdos"
+      />
+      <meta name="author" content="Silas Henrique Alves Araújo" />
+      <meta property="og:title" content="ALIS" />
+      <meta
+        property="og:description"
+        content="O Agregador de Locais Inclusivos para Surdos"
+      />
+      <meta property="og:image" content="/card.png" />
+      <meta property="og:url" content="https://alis.silash35.vercel.app/" />
+      <meta name="twitter:card" content="/card.png" />
+
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
         href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap"
