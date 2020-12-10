dcac42130b63b2e62c110931a7cda07cb93cba11
Silas Henrique <silash35@gmail.com>
2020-12-10 18:26:35 -0300

    Now love is a inline svg as a react component

--- src/pages/sobre.js
+++ src/pages/sobre.js
@@ -1,19 +1,8 @@
-import styles from "../styles/card.module.scss";
+import styles from "../styles/horizontalCard.module.scss";
 import DefaultHead from "../components/defaultHead";
 import Header from "../components/header";
 import Title from "../components/title";
-import Image from "next/image";
-
-const style = {
-  display: "flex",
-  flexDirection: "row",
-  flexWrap: "wrap",
-
-  justifyContent: "center",
-  alignItems: "center",
-  width: "auto",
-  height: "auto",
-};
+import Love from "../components/loveIcon";
 
 export default function sobre(props) {
   return (
@@ -27,25 +16,20 @@ export default function sobre(props) {
       <main>
         <Title>
           <h1>
-            O que é o <span>ALIS</span>?
+            O que é o <span>ALIS</span>
           </h1>
           <p>O Agregador de Locais Inclusivos para Surdos</p>
         </Title>
 
-        <article className={styles.card} style={style}>
-          <p style={{ textAlign: "justify", maxWidth: "400px" }}>
+        <article className={styles.card} id={styles.horizontalCard}>
+          <p>
             O ALIS é uma plataforma voltada para a comunidade surda a fim de minimizar as
             dificuldades desse público na procura de locais inclusivos. Aqui você pode contribuir
             registrando um lugar que você foi e tinha uma tecnologia pra facilitar a comunicação com
             surdos ou interprete.
           </p>
 
-          <Image
-            src="/love.svg"
-            alt="Ícone de Amor em libras com um coração"
-            width={250}
-            height={250}
-          />
+          <Love />
         </article>
       </main>
     </React.Fragment>
