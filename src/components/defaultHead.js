933d16c93f593d11ad467b6e079656fbed3c98fc
Silas Henrique <silash35@gmail.com>
2021-02-23 17:08:14 -0300

    fix head and icons

--- src/components/defaultHead.js
+++ src/components/defaultHead.js
@@ -3,40 +3,48 @@ import Head from "next/head";
 export default function DefaultHead(props) {
   return (
     <Head>
-      {/* usual meta tags */}
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1" />
-      <meta httpEquiv="content-type" content="text/html" charSet="UTF-8" />
-      <meta name="description" content="O Agregador de Locais Inclusivos para Surdos" />
-      <meta name="author" content="Silas Henrique Alves Araújo" />
+      {/* Primary Meta Tags */}
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="content-type" content="text/html" />
 
-      {/* Cards */}
-      <meta property="og:title" content="ALIS" />
-      <meta property="og:description" content="O Agregador de Locais Inclusivos para Surdos" />
-      <meta property="og:image" content="/card.png" />
-      <meta property="og:image:alt" content="ALIS logo: um texto azul escrito alis" />
+      {/* SEO Meta Tags */}
+      <meta name="author" content="Silas Henrique Alves Araújo" />
+      <meta name="description" content="O Agregador de Locais Inclusivos para Surdos" />
+
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
       <meta property="og:url" content="https://alis.vercel.app/" />
+      <meta property="og:title" content="ALIS" />
+      <meta property="og:image" content="https://alis.vercel.app/card.png" />
+      <meta property="og:image:alt" content="A letra 'a' azul e cursiva, que a um olhar atento lembra uma orelha" />
+      <meta property="og:locale" content="pt_BR" />
+      <meta property="og:description" content="O Agregador de Locais Inclusivos para Surdos" />
+
+      {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://alis.vercel.app/tCard.png" />
+      <meta name="twitter:url" content="https://alis.vercel.app/" />
+      <meta name="twitter:title" content="ALIS" />
+      <meta name="twitter:image" content="https://alis.vercel.app/card.png" />
       <meta name="twitter:creator" content="@silash35" />
+      <meta name="twitter:description" content="O Agregador de Locais Inclusivos para Surdos" />
+
+      {/* Icons */}
+      <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+      <link rel="manifest" href="/icons/site.webmanifest" />
+      <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#0070f3" />
+      <link rel="shortcut icon" href="/icons/favicon.ico" />
+      <meta name="apple-mobile-web-app-title" content="ALIS" />
+      <meta name="application-name" content="ALIS" />
+      <meta name="msapplication-TileColor" content="#2d89ef" />
+      <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+      <meta name="theme-color" content="#ffffff" />
 
       {/* Fonts */}
       <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap"
-        rel="stylesheet"
-      />
-
-      {/* PWA */}
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-      <meta name="msapplication-TileColor" content="#2d89ef" />
-      <meta name="theme-color" content="#ffffff" />
-
-      <link rel="manifest" href="/manifest.json" />
+      <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet" />
 
       {/* etc */}
       {props.children}
