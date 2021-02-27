2815f50d028105315086c5dc6f78b50c70b0e189
Silas Henrique <silash35@gmail.com>
2021-02-27 16:06:22 -0300

    start to develop images feature

--- src/components/place/index.js
+++ src/components/place/index.js
@@ -1,15 +1,15 @@
+import Image from "next/image";
+
 import PlaceEdit from "../placeEdit";
 import styles from "./place.module.scss";
 
 export default function Place({ place }) {
   return (
-    <article className={styles.card}>
-      <h2>{place.name}</h2>
-      <section>
-        <p>Descrição: {place.description}</p>
-        <p>Endereço: {place.address}</p>
-      </section>
-      <PlaceEdit id={place._id} />
+    <article className={styles.card} style={{ backgroundImage: "url(/images/empty.png)" }}>
+      <div className={styles.content}>
+        <h2>{place.name}</h2>
+        <PlaceEdit id={place._id} />
+      </div>
     </article>
   );
 }
