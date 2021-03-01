64d03f143a2141560b027bbada64256884051bc3
Silas Henrique <silash35@gmail.com>
2021-03-01 20:00:06 -0300

    add theme for card

--- src/components/place/index.js
+++ src/components/place/index.js
@@ -1,11 +1,17 @@
-import Image from "next/image";
+import { useContext } from "react";
 
+import { ThemeContext } from "../../contexts/ThemeContext";
 import PlaceEdit from "../placeEdit";
 import styles from "./place.module.scss";
 
 export default function Place({ place }) {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <article className={styles.card} style={{ backgroundImage: "url(/images/empty.png)" }}>
+    <article
+      className={`${styles.card} ${styles[theme]}`}
+      style={{ backgroundImage: "url(/images/empty.png)" }}
+    >
       <div className={styles.content}>
         <h2>{place.name}</h2>
         <PlaceEdit id={place._id} />
