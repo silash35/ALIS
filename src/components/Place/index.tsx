2d6dba47a2a12c23cf79baa1d9856dd09a766e7d
Silas Henrique <silash35@gmail.com>
2021-03-19 15:21:12 -0300

    now the place card is a link to place page

--- src/components/Place/index.tsx
+++ src/components/Place/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useContext } from "react";
 
 import { ThemeContext } from "@/contexts/ThemeContext";
@@ -14,14 +15,18 @@ export default function Place({ place }: Props) {
   const { theme } = useContext(ThemeContext);
 
   return (
-    <article
-      className={`${styles.card} ${styles[theme]}`}
-      style={{ backgroundImage: "url(/images/empty.png)" }}
-    >
-      <div className={styles.content}>
-        <h2>{place.name}</h2>
-        <PlaceEdit id={place._id} />
-      </div>
-    </article>
+    <Link as={`/locais/${place._id}`} href={`/locais/${place._id}`}>
+      <a>
+        <article
+          className={`${styles.card} ${styles[theme]}`}
+          style={{ backgroundImage: "url(/images/empty.png)" }}
+        >
+          <div className={styles.content}>
+            <h2>{place.name}</h2>
+            <PlaceEdit id={place._id} />
+          </div>
+        </article>
+      </a>
+    </Link>
   );
 }
