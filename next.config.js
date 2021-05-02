44e61cf21f7096977cf58abed31d1dc5e50f4622
Silas Henrique <silash35@gmail.com>
2021-05-02 16:59:38 -0300

    fix sass imports

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
-/* global require, module, process */
+/* global require, module, process, __dirname */
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require("next-pwa");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
 
 module.exports = withPWA({
   pwa: {
     dest: "public",
     disable: process.env.NODE_ENV === "development",
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/styles")],
   },
   future: {
     webpack5: true,
