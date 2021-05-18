ad3be635c283cecc5ba121709c8f3caa715c16b3
Silas Henrique <silash35@gmail.com>
2021-05-18 16:06:10 -0300

    add share links

--- src/components/PlaceInformation/ShareButton.tsx
+++ src/components/PlaceInformation/ShareButton.tsx
@@ -6,10 +6,10 @@ import IconButton from "@material-ui/core/IconButton";
 import Input from "@material-ui/core/Input";
 import CloseIcon from "@material-ui/icons/Close";
 import FacebookIcon from "@material-ui/icons/Facebook";
-import InstagramIcon from "@material-ui/icons/Instagram";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import ShareIcon from "@material-ui/icons/Share";
 import TwitterIcon from "@material-ui/icons/Twitter";
+import WhatsAppIcon from "@material-ui/icons/WhatsApp";
 import { useRef, useState } from "react";
 
 import styles from "./ShareButton.module.scss";
@@ -18,13 +18,27 @@ const ShareButton = () => {
   const [open, setOpen] = useState(false);
   const InputRef = useRef<HTMLInputElement>(null);
 
+  const placeUrl = typeof window !== "undefined" ? window.location.href : "";
+  const placeText = "Conheça esse local";
+  const encodedPlaceURL = encodeURIComponent(placeUrl);
+  const encodedPlaceText = encodeURI(placeText);
+
+  const facebookUrl = `https://www.facebook.com/sharer.php?u=${encodedPlaceURL}`;
+  const twitterUrl = `https://twitter.com/share?url=${encodedPlaceURL}&text=${encodedPlaceText}`;
+  const whatsAppUrl = `https://wa.me/?text=${encodedPlaceText} ${encodedPlaceURL}`;
+  const linkedInUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${encodedPlaceURL}&title=${encodedPlaceText}`;
+
+  const goToUrl = (url: string) => {
+    window.open(url, "_blank");
+  };
+
   const openDialog = async () => {
     if (typeof navigator.share !== "undefined") {
       try {
         await navigator.share({
           title: "ALIS",
-          text: "Conheça esse local",
-          url: typeof window !== "undefined" ? window.location.href : "",
+          text: placeText,
+          url: placeUrl,
         });
       } catch {
         // do nothing
@@ -68,16 +82,36 @@ const ShareButton = () => {
         </DialogTitle>
         <DialogContent dividers>
           <div className={styles.container}>
-            <IconButton aria-label="Compartilhar no Facebook">
+            <IconButton
+              onClick={() => {
+                goToUrl(facebookUrl);
+              }}
+              aria-label="Compartilhar no Facebook"
+            >
               <FacebookIcon fontSize="large" />
             </IconButton>
-            <IconButton aria-label="Compartilhar no Twitter">
+            <IconButton
+              onClick={() => {
+                goToUrl(twitterUrl);
+              }}
+              aria-label="Compartilhar no Twitter"
+            >
               <TwitterIcon fontSize="large" />
             </IconButton>
-            <IconButton aria-label="Compartilhar no Instagram">
-              <InstagramIcon fontSize="large" />
+            <IconButton
+              onClick={() => {
+                goToUrl(whatsAppUrl);
+              }}
+              aria-label="Compartilhar no WhatsApp"
+            >
+              <WhatsAppIcon fontSize="large" />
             </IconButton>
-            <IconButton aria-label="Compartilhar no LinkedIn">
+            <IconButton
+              onClick={() => {
+                goToUrl(linkedInUrl);
+              }}
+              aria-label="Compartilhar no LinkedIn"
+            >
               <LinkedInIcon fontSize="large" />
             </IconButton>
           </div>
@@ -85,7 +119,7 @@ const ShareButton = () => {
             <Input
               autoFocus
               margin="dense"
-              defaultValue={typeof window !== "undefined" ? window.location.href : ""}
+              defaultValue={placeUrl}
               readOnly
               fullWidth
               inputRef={InputRef}
