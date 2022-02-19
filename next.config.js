4f395510d2543a02cae98bc5847017d60c0c9a62
Silas Henrique <silash35@gmail.com>
2022-02-19 20:36:47 -0300

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,41 @@
-/* global require, module, process, __dirname */
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-env node */
 const withPWA = require("next-pwa");
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require("path");
 
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, "src/styles")],
-  },
-  future: {
-    webpack5: true,
-  },
-});
+module.exports = () => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+
+  let nextConfig = {
+    pwa: {
+      dest: "public",
+      disable: process.env.NODE_ENV === "development",
+    },
+    sassOptions: {
+      includePaths: [path.join(__dirname, "src/styles")],
+    },
+    compiler: {},
+  };
+
+  if (process.env.NODE_ENV === "production") {
+    nextConfig.compiler.removeConsole = true;
+  }
+
+  if (process.env.KEEP_PROPERTIES !== "true") {
+    nextConfig.compiler.reactRemoveProperties = {
+      properties: ["^data-test$", "^data-testid$"],
+    };
+  }
+
+  if (process.env.NODE_ENV !== "development") {
+    nextConfig.pwa = {
+      dest: "public",
+    };
+
+    nextConfig = withPWA(nextConfig);
+  }
+
+  return nextConfig;
+};
